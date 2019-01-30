ef728e620df8a6b82e874cb417d8872022c7f9ef
duyenle <lehuuduyen96@gmail.com>
2019-01-30 15:18:15 +0700

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-30314888-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-30314888-2');
+    </script>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta property='og:locale' content='en_US'/>
+    <meta property='og:title' content='Get Facebook Android Token by Loc Mai'/>
+    <meta property='og:description'
+          content='Chrome Extension, Software, Tools, Web Application, Software Engineer, Web Developer'/>
+    <meta property="og:site_name" content='Get Facebook Android Token by Loc Mai'/>
+    <meta property='og:type' content='website'/>
+    <meta property="og:url" content="https://code.lnmai.com/"/>
+    <meta property='og:image' content='https://i.imgur.com/qmYIVwJ.png'/>
+    <meta property="og:image:alt" content="Hire Loc!"/>
+    <meta property='fb:app_id' content='242443032981840'/>
+    <meta property='fb:admins' content='806399409'/>
+    <meta property='fb:pages' content='906140862921618'/>
+    <meta property='twitter:image' content='https://i.imgur.com/qmYIVwJ.png'/>
+    <meta name='twitter:site' content='@locmai0808'/>
+    <meta name='twitter:domain' content='lnmai.com'/>
+    <meta name='twitter:creator' content='@locmai0808'/>
+    <meta name='twitter:card' content='summary_large_image'/>
+    <meta name='copyright' content='Get Facebook Android Token by Loc Mai'/>
+    <meta name='keywords'
+          content='Loc Mai, lnmai, Software Engineer, Web Developer, Facebook Privacy Changer, Facebook Messages Counter, Facebook Messages Downloader, Facebook Interaction Scanner, Facebook Profile Picture'/>
+    <meta name='google-site-verification' content='4fswIVcvk6UH22j-b1Qe2yWXDIIu9FkxFt_YKWV0CgA'/>
+
+    <link rel="apple-touch-icon" href="{{url('')}}/img/icon.jpg">
+    <link rel="icon" href="{{url('')}}/img/icon.jpg">
+    <title>Loc Mai - Get Facebook Access Token</title>
+
+    <link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="{{url('')}}/css/material-dashboard.css?v=2.0.1">
+
+
+</head>
