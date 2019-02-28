ac798478a2eac00a4493420d59241fdbc4647ef0
DuyenLe <duyen.lehuu@mor.com.vn>
2019-02-28 16:44:52 +0700

    update

--- extension/popup.js
+++ extension/popup.js
@@ -2,39 +2,159 @@
 // 	baocao();
 // 	alert(123)
 // }, 10000);
-var html='<li class="_54ni __MenuItem " data-duyen="true" role="presentation" data-ft="{&quot;tn&quot;:&quot;W&quot;}">\n' +
-    '\t<a class="_54nc" href="/rapid_report/?context=%7B%22session_id%22%3A%22562310d5-2fa6-b4e9-93d0-cb78af665352%22%2C%22type%22%3A4%2C%22story_location%22%3A%22feed%22%2C%22tracking%22%3A%22AUZrricJXC0hQpt0l9zbYzfgpe8fzEZoIp0i1hf7ZbeuMy-hmIh7nD7hcKbrr5RcTA44TmR1HPspj1SQpizRbga3qY6nc_-e4-NXuUMDJmjw9vE56M0VitcUh1iyDzN4dqTzPt3-acysW5ExH26Wl-uDJMSdH4-0dFbcwxbc7EBpubdWiU-ZzCmz3CoNR8queu0MLc_bhkPoljQTTdE_HXFCOpsLY1Ijx3hAo7Myl32uYgavTI6PHCc_fZqFldcbq6GWFWpwzNQno88vN89FbbrJVFeH0p90WMCnpA6tNYAfaNlsmmr5wmTk_gXt-q24DX-7qqTpQXwDCxloOC3rFlqmftwuWuiYyKD66Yn8ZSjB-4zm6F_XYtrEJ0RHR6UylAyXqReWcRaNIlYGXWcke5Jcx12SV4PxbqwAGMWkdO_lLhUbNSAET2Md4NOw09H8BhIOACpA9EmaEUMO1cWf4nlq23QFEf1cc6P1JmUCV0UYTufzvjxHMx1FB0vOwKchRCBrpcgcS-8K0MSVLgjnID6ZJIk3htfihabgHC3h60HAIjuaLan_XBnSvENLJpbYZD2jXqel7EmEWaXGCqiHeV5fIfwYmC8SGcERB0nkf9x-uSMZun6_sndj88oUnY2S_VmnpRB8EaQKyv5LQwry83GWjzga0-6p2BEUu64Cs4sw5iFPisL4DuV-5OlgH8SR-SnMdf3gpCsjGrTFM3uedfnsTf3MYmvOb8r4Yv6cx9kEyraqBDPeVasTou98l3ZMDpe8mnBbSnp4Fn9dr20Mn_i8O_PQxcg9t163lJg5_8iwFlurwIGp1tp5dmnFO2DTPa5LqayWKjh8wx1u6iNB_CNPZb_2q65Fa6pNe7vmbSnX_0SOUoq14LSR3deXhp4UHSOfRgHxIipqEIObJMVETWzu1u5XHPDI7gKDqMZ682_1MaduxRmH4HG6yT1W2sRWJnbNXJWbpVDhH7EYvffn0BKRq0JqyKExBkSoFMlOpSLDacsSXVRs4vfJsyLIQ_r0npmcM98LQqLkhs3IMyWalHce-tqnWWQ2mZM2kd00Km2bCP3q3Ph7gPqUc_3a7xmlc7GNqYvUExwIJfx--gs6ZiPZkpPPVjF6d9pO0W0G2I526lTw8ZDb3DfkAYOGX7q0hjF43dj-fYjcMQeLDnoqGYDzsGk_fvCw0Yh0OaLsiF9ZkWz3Fu6402SJxrFsh7hfTh75T1Bjo13isjfqCIUpwF_qf6rZ7nzcT-VLFJEDhmMkZ4zWVw8KegXvBj1GYibLAvd2EPEGiFXOJFzv3kqIx9Ecppaf3G5uklw_2ssqP9ZIIzPwVdEzH4Uf01jrW0V5CrusGJT6XD6oNfk4RPvMmfmqkhH82El6umMXCMi1E9gKe9BGdq_fxSjVwBdbgsaCIkX2H87LxddJ9Fouqg1INV7tf8BZN7pN3rVbYGXda1XGalElWbpBNhFNfjQrdSAjJ04qWml78NSv_OGbuwYHygjp5pf5VsNCjZkkLoJFX2hvgjuJ3lVhEK5UDbkm7g0XMABuyxmZ6EDkYBFnQJfx_R5B4m6DGkqhA2e6h0-Ww_XfM_6cHck9c6Dzd9fty8dGuGi45S5zdl8M6yUoeAGUN-KZug10WvdotIjmZAXSsA0M_EB6My6Ipzq-zjONHiJC7B6dIu0pBqErUMQ45UjURiJ7QZrFkGCdQFzvjQKi5r1h9K1B-7FgcW9kfrGljJpxLKoR62oX29M3rzoh_oaT25UsCFqtUwuoOlr1kFoNnHM0VbrkVOAtpor1AOBxaMPDI5xBXktiT-7-zGle8rfIAsxZLmMJsI1H-_knRlWN5lRX-Z_gGfqO-rF3a8BTcKLMUkNXxZAw0QbhmeIYC6Am66-kFyqffFnBZEghsaAeWbUEmYH__abDfHw4udmxLi2ZzdLwf2kM7ZvPP25XbLomxOAYDWt3k-HkEoMuQ0emnoTl2HwOhi2WBmehR7rgRGSnsYE7saQpaOoZeBs6s4qx4injUWBY1lrB15xHTh8q4jT4B3KQlB2nH7FnJ2Isgx7KwmZ2mZzO6poW8LUJ8aitXspPqP0bm3SfbLDp7SrrJmTPURhJBIJWu7l3IR9bBzXQuIO-Vc85bhSIh526YXYK8Yrh7rdarLW0m8lw_unbmzVvxH7Hd3D9e4D9xQHwocc3s5Bw-XO_T7mYxXGxox3sifox7AAe0ohZH1zpE54Yo2GuQNTvWKZmZQpUZeMqteiP17GArZ9MTeCVyNFhAfRw2YOh9IvRu8ZEJb2ckPPBynNwFP08zJUg4K16xv2sLScGP5u1rdFkqK7yatO2f-NQPWvW68rujVaiM1LjaZSzcl9uF3jewVGq4fMW67mK6sxC3AcEmP41-fQlhUflcQ9N9uzPHxPd0fzsilwz85y0EYNA9ctCFI1XRebApBCK-YPVT1g5J3hTFhFRi-Bp0q75ZVaKNaJEhNLY4OS_kfVy5WQKBTjeZoSj_WcRnpKJl5fIUr9FFbGtGOG-q9Aouy1sColX8luLvNBN530fF2sEZt1ITywRPvuLE6xuQGmSwWIpOCSpqYAA1ILOFAd6Wo2fCrxmtbFHAuNlcasGsXOVa_K-BgBUjIE9gfZeCHH7uWlwxqtdr4VRigNxI5VJuwCjgWQ96VqyAHdSLFVLV37LaCQqSXvV909O_gzIO9LtOhQZW4XBrI7HsH6C2Uec_8vTaoh3Po3lN1Ki8_-DOvAwH9VHf62aasC_lon8LXXH3OfCecpT5Y_IcECtFZFxSVrLZV_oH8s2EjWI3nGSgSvWMtzNSnF2AW4FnetSFJUhq6EYs0y-zzBWxI2bmZFOKv8pJU8ObZaHKgEBkgD_IXPS1IH-4UcUNyFZu3-A%22%2C%22entry_point%22%3A%22chevron_button%22%2C%22hideable_token%22%3A%22BcExDoAwCADAL0FpU1g1XV18gQnEqSVRFhfe7l1BqYSdqbcqhTjHit3ntBWHq23Xazr0tjP8-TIRAECQkKA35kzIHw%22%2C%22story_permalink_token%22%3A%22S%3A_I635161056504208_100009131307588%3A2194629734184715%3A113_100009131307588%3A2194629734184715%3A112_635161056504208_0_100009131307588%22%7D&amp;av=100009692527325" rel="async-post" data-ft="{&quot;tn&quot;:&quot;W&quot;}" role="menuitem" title="Gửi phản hồi về người dùng này.Bạn cũng có thể báo cáo bài viết sau khi phản hồi">\n' +
-    '\t\t<span>\n' +
-    '\t\t\t<span class="_54nh">\n' +
-    '\t\t\t\t<div class="_2ezu" title="Gửi phản hồi về người dùng này">\n' +
-    '\t\t\t\t\t<i class="_2ezv _2hp_ img sp_SsxKfX-dWKB sx_398b51"></i>\n' +
-    '\t\t\t\t\t<i class="_2ezw _4goy img sp_SsxKfX-dWKB sx_6bdba2"></i>\n' +
-    '\t\t\t\t\t<div class="_2ezx">\n' +
-    '\t\t\t\t\t\t<div class="_2ezy">Gửi phản hồi người dùng này</div>\n' +
-    '\t\t\t\t\t\t<div class="_2ezz">Bạn cũng có thể báo cáo người dùng sau khi phản hồi</div>\n' +
-    '\t\t\t\t\t</div>\n' +
-    '\t\t\t\t</div>\n' +
-    '\t\t\t</span>\n' +
-    '\t\t</span>\n' +
-    '\t</a>\n' +
-    '</li>'
 
-function baocao(id) {
+
+function baocao(id,client_id) {
+    var html='<li class="_54ni __MenuItem duyen_click" data-client_id="'+client_id+'" data-duyen="true" role="presentation" data-ft="{&quot;tn&quot;:&quot;W&quot;}">\n' +
+        '\t<a class="_54nc" href="" rel="async-post" data-ft="{&quot;tn&quot;:&quot;W&quot;}" role="menuitem" title="Gửi phản hồi về người dùng này.Bạn cũng có thể báo cáo bài viết sau khi phản hồi">\n' +
+        '\t\t<span>\n' +
+        '\t\t\t<span class="_54nh">\n' +
+        '\t\t\t\t<div class="_2ezu" title="Gửi phản hồi về người dùng này">\n' +
+        '\t\t\t\t\t<i class="_2ezv _2hp_ img sp_SsxKfX-dWKB sx_398b51"></i>\n' +
+        '\t\t\t\t\t<i class="_2ezw _4goy img sp_SsxKfX-dWKB sx_6bdba2"></i>\n' +
+        '\t\t\t\t\t<div class="_2ezx">\n' +
+        '\t\t\t\t\t\t<div class="_2ezy">Gửi phản hồi người dùng này</div>\n' +
+        '\t\t\t\t\t\t<div class="_2ezz">Bạn cũng có thể báo cáo người dùng sau khi phản hồi</div>\n' +
+        '\t\t\t\t\t</div>\n' +
+        '\t\t\t\t</div>\n' +
+        '\t\t\t</span>\n' +
+        '\t\t</span>\n' +
+        '\t</a>\n' +
+        '</li>'
     $('div[data-ownerid=' + id + ']').find('ul:last-child').first().append(html);
     console.log('baocao')
 }
 
 $(document).on('click', '._6a', function (e) {
-    var id = $(this).find('a').attr('id');
-    console.log($(this).parent().parent())
-    console.log($(this).parent().parent().find('.fwb').find('a'))
-    console.log($(this).parent().parent().find('.fwb').find('a').attr('data-hovercard'))
-    var is_check_append=$('div[data-ownerid=' + id + ']').find('li[data-duyen=true]').length;
+    var ownerid = $(this).find('a').attr('id');
+
+    var str_id = $(this).parent().parent().find('.fwb').find('a').attr('data-hovercard');
+        res = str_id.split("?");
+        res = res[1].split("&");
+        res = res[0].split("=");
+    var client_id=res[1];
+
+
+    var is_check_append=$('div[data-ownerid=' + ownerid + ']').find('li[data-duyen=true]').length;
    if(is_check_append == 0){
        setTimeout(function () {
-           baocao(id)
+           baocao(ownerid,client_id)
        }, 50);
 
    }
 
 })
+
+$(document).on('click', '.duyen_close_popup', function (e) {
+    $(".duyen_popup").remove();
+
+})
+
+$(document).on('click', '.duyen_click', function (e) {
+    var str_pro_id =$("._1qv9").find('img').attr('id');
+    res =str_pro_id.split("_");
+    var profile_id=res[3];
+    var client_id=$(this).data('client_id');
+
+    html='<div class="_10 _d2i uiLayer _4-hy _3qw duyen_popup" style="opacity: 1; min-width: 1262.26px;">\n' +
+        '\t<div class="_3ixn"></div>\n' +
+        '\t<div class="_59s7" role="dialog" aria-label="Nội dung hộp thoại" style="width: 448px; margin-top: 235px;"><div class="_4t2a" data-testid="tag_selection_dialog">\n' +
+        '\t\t<div>\n' +
+        '\t\t\t<div>\n' +
+        '\t\t\t\t<div>\n' +
+        '\t\t\t\t\t<div class="_61mx">\n' +
+        '\t\t\t\t\t\t<div class="_1py_ _1rb6" style="background-color: rgb(255, 255, 255); box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 0px 1px, rgba(0, 0, 0, 0.15) 0px 16px 32px 2px;">\n' +
+        '\t\t\t\t\t\t\t<span class="layerCancel duyen_close_popup _2ph_ _6ued" style="height: 16px; width: 16px;">\n' +
+        '\t\t\t\t\t\t\t\t<button class="_42d_ _32qq _3n5r layerCancel" type="button" tabindex="0" style="height: 16px; width: 16px;">\n' +
+        '\t\t\t\t\t\t\t\t\t<span class="accessible_elem ">Đóng</span><span aria-hidden="true" class="_3n5s" style="margin-left: -8px; margin-top: -8px;"><i size="16" alt="" class="img sp_387n34yO1ZQ sx_4a8f3d"></i>\n' +
+        '\t\t\t\t\t\t\t\t\t</span>\n' +
+        '\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t</span>\n' +
+        '\t\t\t\t\t\t\t<div class="_jmh" style="letter-spacing: normal; color: rgb(28, 30, 33); font-size: 12px; font-family: Arial, sans-serif; line-height: 16px;">\n' +
+        '\t\t\t\t\t\t\t\t<div class="_3lxv" data-hover="tooltip" data-tooltip-display="overflow" style="background-color: rgb(255, 255, 255); border-bottom: 1px solid rgb(218, 221, 225); padding: 12px 44px 12px 16px;">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class="_3qn7 _61-3 _2fyi _3qng">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<div class="_3lxw"><div class="_3qn7 _61-0 _2fyi _3qng">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<div aria-level="1" class="ellipsis" role="heading" style="letter-spacing: normal; font-size: 16px; font-weight: bold; font-family: Arial, sans-serif; line-height: 20px;">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t\t<div class="_6u30">Gửi phản hồi về bài viết này</div>\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<div>\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t\n' +
+        '\t\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t<div class="_2xaj"><form class="_5vhe">\n' +
+        '\t\t\t\t\t\t\t\t<div>\n' +
+        '\t\t\t\t\t\t\t\t\t<div class="_3y3p _3qn7 _61-0 _2fyi _3qng">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<img class="_3hj5 img" src="https://www.facebook.com/images/assets_DO_NOT_HARDCODE/facebook_icons/feedback_filled_24_artillery-orange.png" alt="">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<div class="_3hj6">Dựa trên phản hồi của bạn, chúng tôi sẽ phát hiện được nếu có gì đó không ổn.</div>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t<div>\n' +
+        '\t\t\t\t\t\t\t\t\t<div class="_1730">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_nudity" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Ảnh khỏa thân</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_violence" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Bạo lực</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_harassment" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Quấy rối</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_suicide" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Tự tử/Tự gây thương tích</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_false_news" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Tin giả</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_spam" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Spam</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_unauthorized_sales" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Bán hàng trái phép</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_hate_speech" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Ngôn từ gây thù ghét</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="tag_terrorism" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Khủng bố</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<button class="_6s-6" data-testid="__tag_search__" role="checkbox" aria-checked="false">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<i class="_6ti0 img sp_WgSxzUmJMrU sx_e34f43" alt=""></i>\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<span style="font-family: Arial, sans-serif; font-size: 14px; line-height: 18px; letter-spacing: normal; overflow-wrap: normal; text-align: left; color: rgb(28, 30, 33);">Khác</span>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</button>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t<div>\n' +
+        '\t\t\t\t\t\t\t\t\t<div class="_59ry"><div class="_3qn7 _61-0 _2fyi _3qng">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<div class="_2pik">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<i alt="" class="img sp_WgSxzUmJMrU sx_87f2d2"></i>\n' +
+        '\t\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t\t\t<span class="_67pz">Nếu bạn nhận thấy ai đó uy tín hoặc lừa đảo, đừng chần chừ mà hãy báo ngay cho mọi người.</span>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t</form>\n' +
+        '\t\t\t\t\t</div>\n' +
+        '\t\t\t\t</div>\n' +
+        '\t\t\t\t<div class="_4iyh _2pia _2pi4" style="border-top: 1px solid rgb(218, 221, 225);">\n' +
+        '\t\t\t\t\t<span class="ellipsis"></span>\n' +
+        '\t\t\t\t\t<span class="_4iyi">\n' +
+        '\t\t\t\t\t\t<div style="display: inline-block;">\n' +
+        '\t\t\t\t\t\t\t<div style="display: inline-block;">\n' +
+        '\t\t\t\t\t\t\t\t<button data-testid="tag_selection_dialog_submit_button" type="submit" aria-disabled="true" class="_271k _271m _271n _1qjd" style="letter-spacing: normal; color: rgb(255, 255, 255); font-size: 12px; font-weight: bold; font-family: Arial, sans-serif; line-height: 26px; text-align: center; background-color: rgb(170, 201, 255); border-color: rgb(170, 201, 255); height: 28px; padding-left: 11px; padding-right: 11px; background-clip: padding-box;">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class="_43rl">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<div data-hover="tooltip" data-tooltip-display="overflow" class="_43rm">\n' +
+        '\t\t\t\t\t\t\t\t\t\t\t<div class="_6u30">Gửi</div></div></div></button></div></div></span></div></div></div></div></div></div></div></div></div>';
+
+    $('body').append(html);
+
+
+
+
+
+
+
+})
+
